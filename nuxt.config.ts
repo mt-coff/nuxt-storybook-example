4882eafa4847952e2b2cfb803a633cb41b108007
mt-coff <mt.coff.00@gmail.com>
2019-08-26 16:32:39 +0900

    use core-js@3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -44,6 +44,19 @@ export default {
    ** Build configuration
    */
   build: {
+    babel: {
+      presets({ isServer }: { isServer: boolean }) {
+        return [
+          [
+            require.resolve('@nuxt/babel-preset-app'),
+            {
+              buildTarget: isServer ? 'server' : 'client',
+              corejs: { version: 3 }
+            }
+          ]
+        ]
+      }
+    },
     /*
      ** You can extend webpack config here
      */
